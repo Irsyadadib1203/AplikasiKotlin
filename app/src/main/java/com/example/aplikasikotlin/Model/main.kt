4307f3b343a8e6ad4fae8d14661d5d1269b2bef2
irsyadadib1203 <irsyadadib1234@gmail.com>
2024-03-04 10:32:01 +0700

    Class and object

---
+++ app/src/main/java/com/example/aplikasikotlin/Model/main.kt
@@ -0,0 +1,20 @@
+package com.example.aplikasikotlin.Model
+
+import com.example.aplikasikotlin.Model.Aquarium
+fun buildAquarium() {
+    val aquarium1 = Aquarium()
+    aquarium1.printSize()
+    // default height and length
+    val aquarium2 = Aquarium(width = 25)
+    aquarium2.printSize()
+    // default width
+    val aquarium3 = Aquarium(height = 35, length = 110)
+    aquarium3.printSize()
+    // everything custom
+    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+    aquarium4.printSize()
+}
+
+fun main() {
+    buildAquarium()
+}
